f84f1240809f6367abad78b1e1ca0b5074458a31
KhaledMaher024 <44631332+KhaledMaher024@users.noreply.github.com>
2019-06-28 23:11:06 -0700

    Add files via upload

---
+++ exercise2/src/main/java/at/tuwien/ds/madmp/MadmpApplication.java
@@ -0,0 +1,13 @@
+package at.tuwien.ds.madmp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class MadmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadmpApplication.class, args);
+	}
+
+}
